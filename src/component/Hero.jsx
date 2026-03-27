30248e2ea988151bb1239a1df00d1820546586b9
LEGENDANISH <0504anish@gmail.com>
2026-03-28 02:12:41 +0530

    added the perfect hero layout

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -2,36 +2,40 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="bg-white ">
-      <div>
-        <div className="grid grid-cols-3 gap-4">
-             {/* left  */}
-          <div className="grid col-span-1">
+    <div className="bg-white">
+      <div className="grid grid-cols-3 gap-0 mt-15">
+        {/* LEFT */}
+        <div className="flex flex-col items-center gap-4">
+          <img
+            className="ml-[280px] h-90 rounded-xl"
+            src="./image1.jpg"
+            alt="img1"
+          />
+          <img
+            className="ml-[380px] h-45 rounded-xl"
+            src="./image2.jpg"
+            alt="img2"
+          />
+        </div>
 
-            <div className="flex flex-col items-center gap-2.5">
-              <img
-                className="ml-[280px] h-90 rounded-xl mt-15"
-                src="./image1.jpg"
-                alt="img1"
-              />
-              <img
-                className="ml-[380px] h-45 rounded-xl mt-2"
-                src="./image2.jpg"
-                alt="img1"
-              />
-            </div>
-          </div>
-             {/* Center — big single image */}
-          <div className="grid col-span-2">
-            <img
-              className="flex mt-20 h-120 rounded-2xl ml-10"
-              src="./image3.jpg"
-            />
-          </div>
-          <div className="grid col-span-3">
-                {/* right  */}
-                
-          </div>
+        {/* CENTER */}
+        <div className="flex justify-center mt-10">
+          <img className="h-120 rounded-2xl" src="./image3.jpg" alt="img3" />
+        </div>
+
+        {/* RIGHT */}
+        <div className="flex flex-col items-start gap-4 -translate-x-80">
+          {" "}
+          <img
+            className="ml-[280px] h-90 rounded-xl"
+            src="./image4.jpg"
+            alt="img4"
+          />
+          <img
+            className="ml-[380px] h-45 rounded-xl -translate-x-25"
+            src="./image5.jpg"
+            alt="img5"
+          />
         </div>
       </div>
     </div>
